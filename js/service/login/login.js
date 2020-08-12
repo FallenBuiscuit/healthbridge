2908e8d582e1f94dbac5b0f405d9b7e4f85f7b2e
Maria Tia <matiavallecera@gmail.com>
2020-08-12 14:16:36 +0800

    new commit

--- js/service/login/login.js
+++ js/service/login/login.js
@@ -20,14 +20,8 @@ loginapp.controller("loginServiceCtrl", function($scope,$http){
 
         $http(request).then(function(response){
             if(response.status === 200){
-                var url = require('url');
-                var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
-                var q = url.parse(adr, true);
-
-                console.log(q.host); //returns 'localhost:8080'
-                console.log(q.pathname); //returns '/default.htm'
-
-
+                alert("success!");
+                alert(response.data[0].USERNAME);
             }
         });
     }
