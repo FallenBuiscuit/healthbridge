f48238f479968a94a6c3f8af4c592c650775c4a9
FallenBuiscuit <carloruste@gmail.com>
2020-08-13 14:37:49 +0800

    Fix transition

--- js/service/main/main-service.js
+++ js/service/main/main-service.js
@@ -1,9 +1,46 @@
 var mainapp = angular.module("mainapp", []);
-
 mainapp.controller("mainServiceCtrl", function($scope){
 
     $scope.redirect = function(redirectUrl){
-        $('.health-bridge-loading').show();  
+        $('.health-bridge-loading').show();
+        if(redirectUrl === '/guest'){
+            localStorage.setItem("isLoggedIn", false);
+        }
         window.open("/healthbridge" + redirectUrl, "_self");
     }
 });
+
+
+(function($) {
+    
+    MAPP = {
+        init: function() {
+            this.initialize();
+            this.checkIfLoginOrLoggedOut();
+        },
+
+        initialize: function() {
+            $('.redirect-btn').on("click", function(){
+                var redirectUrl = $(this).val();
+                window.open("/healthbridge" + redirectUrl, "_self");
+            });
+        },
+
+        checkIfLoginOrLoggedOut: function() {
+            var isLoggedIn = localStorage.getItem("isLoggedIn");
+            if(isLoggedIn === 'true'){
+                $('.logged-in').show();
+                $('.logged-out').hide();
+            } else {
+                $('.logged-out').show();
+                $('.logged-in').hide();
+            }
+        },
+
+    };
+    
+
+    $(document).ready(function() {
+        MAPP.init();
+    });
+})(jQuery);
