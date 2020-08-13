b3518c208626833080daff575abc855f8902f5a6
chrstnlnrd <69579861+chrstnlnrd@users.noreply.github.com>
2020-08-13 12:25:51 +0800

    Update landing.js

--- js/service/landing/landing.js
+++ js/service/landing/landing.js
@@ -1,5 +1,5 @@
 var landingapp = angular.module("landingserviceapp", []);
-console.log("NISUUD1");
+
 landingapp.controller("landingServiceCtrl", function($scope){
 
     $scope.landingPharmacy = function(){
