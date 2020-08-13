096403eb3625f0a336917e569bee172bd8d320c3
Christine Leonardo <imchrisleonardo16@gmail.com>
2020-08-13 11:19:24 +0800

    E-Pharmacy initial stuubed script

--- server.js
+++ server.js
@@ -37,3 +37,7 @@ app.get('/healthbridge/login', function (req, res) {
 app.get('/healthbridge/patient', function (req, res) {
     res.sendfile(__dirname + '/web-content/landing/patient-landing-page.html');
 });
+
+app.get('/healthbridge/pharmacy', function (req, res) {
+    res.sendfile(__dirname + '/web-content/e-pharmacy/pharmacy-landing-page.html');
+});
