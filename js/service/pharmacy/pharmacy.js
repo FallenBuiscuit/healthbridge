bf9e08f120a12089debc0227464d2e4be47fc041
FallenBuiscuit <carloruste@gmail.com>
2020-08-14 10:59:14 +0800

    Fix transition and links on mast header to transition to correct user

--- js/service/pharmacy/pharmacy.js
+++ js/service/pharmacy/pharmacy.js
@@ -39,7 +39,6 @@ function ready() {
 }
 
 function purchaseClicked() {
-    alert('Thank you for your purchase')
     var cartItems = document.getElementsByClassName('cart-items')[0]
     while (cartItems.hasChildNodes()) {
         cartItems.removeChild(cartItems.firstChild)
